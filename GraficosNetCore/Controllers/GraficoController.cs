755a0bf5f5a408062fd97285202374e854160e8b
Victor <VictorCarmona9035@hotmail.com>
2022-07-26 11:24:43 +0200

    1-Creacion de aplicacion net Core Modelo-Vista-Controlador

---
+++ GraficosNetCore/Controllers/GraficoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GraficosNetCore.Controllers
+{
+    public class GraficoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
