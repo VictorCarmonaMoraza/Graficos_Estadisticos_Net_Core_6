f9a7faa1b03875987c444f77c90384685d59b00a
Victor <VictorCarmona9035@hotmail.com>
2022-07-26 12:11:43 +0200

    2-Instalando librerias para hacer graficos

--- GraficosNetCore/Controllers/GraficoController.cs
+++ GraficosNetCore/Controllers/GraficoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace GraficosNetCore.Controllers
 {
@@ -7,6 +9,22 @@ namespace GraficosNetCore.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public string graficoInicial()
+        {
+            //Es necesario para graficos utiluzar un BitmAp
+            Bitmap oBitmap = new Bitmap(800,400);
+            //Lo usamos para guardar el grafico
+            using(MemoryStream ms = new MemoryStream())
+            {
+                oBitmap.Save(ms, ImageFormat.Png);
+                //Obtenemos su array de byte
+                byte[] data = ms.ToArray();
+                //Lo convertimos a base64 
+                return Convert.ToBase64String(data);
+            }
+
         }
     }
 }
---
+++ GraficosNetCore/wwwroot/js/graficoInicio.js
@@ -0,0 +1,9 @@
+﻿
+//Cargar grafico cuando carga la pantalla
+window.onload = function () {
+    //Llamada desde nuestro js a nuestro controller
+    //fetchGet(Controlador/nombremetodo, "tipo que devuelve metodo controlador", function data()
+    fetchGet("Grafico/graficoInicial", "text", function (data) {
+        document.getElementById("imgFoto").src = "data:image/png;base64," + data;
+    })
+}
